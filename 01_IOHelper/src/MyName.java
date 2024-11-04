32e047d0976d9b786ac86acbe71dbc2567f68ef4
Leon Hondyk <leon.hondyk@esg.de>
2024-11-04 17:28:16 +0100

    inital commit

---
+++ 01_IOHelper/src/MyName.java
@@ -0,0 +1,15 @@
+/**
+ * MyName class contains main method and is point of entry into program
+ */
+public class MyName {
+	/**
+	 * Main method prints name to the console, does not return anything
+	 * 
+	 * @param args (command line arguments, empty)
+	 */
+	public static void main(String[] args) {
+		// prints string to the console
+		System.out.print("Mein Name ist Leon Hondyk");
+	}
+
+}
---
+++ 02_Kartenspiel/src/de/esg/ausbildung/honl/game/Card.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package de.esg.ausbildung.honl.game;
+
+/**
+ * 
+ */
+public class Card {
+
+	/**
+	 * 
+	 */
+	public Card() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
